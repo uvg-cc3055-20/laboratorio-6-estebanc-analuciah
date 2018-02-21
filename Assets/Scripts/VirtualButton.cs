d6a2efd5f9f642c7c4ce78fbd67b0e7cca47df66
her17138 <her17138@uvg.edu.gt>
2018-02-21 08:46:47 -0600

    fin -
    creacion virtualbutton
    animación de sonido
    generación apk

--- Assets/Scripts/VirtualButton.cs
+++ Assets/Scripts/VirtualButton.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
+using UnityEngine.UI;
 
-public class VirtualButton : MonoBehaviour {
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
 
+	private VirtualButtonBehaviour virtualBtn;
+	public Animator birdAnim;
 	// Use this for initialization
 	void Start () {
-		
+		virtualBtn = GetComponent<VirtualButtonBehaviour>(); 
+		virtualBtn.RegisterEventHandler(this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour b) {
+		birdAnim.SetTrigger("sing");
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour b) {
 	}
 }
