7009395ffa02b0fa7212258dd28fc8d7fb26a8de
her17138 <her17138@uvg.edu.gt>
2018-02-21 07:49:15 -0600

    add virtualbutton

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
